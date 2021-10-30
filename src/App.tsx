622b8b809d5492c7db6a525646155e6acfc6da99
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-10-30 19:23:41 +0300

    init project 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Stopwatch from './components/Stopwatch';
-
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <Stopwatch />
+
     </div>
   );
 }
